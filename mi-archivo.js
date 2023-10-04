84dc64b0d7e880bb95988b296b0333f902a2c746
Daniel Vega Marvez <87838943+Chuz047@users.noreply.github.com>
2023-10-03 18:33:49 -0600

    Segundo commit al repositorio

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,3 +1,4 @@
 console.log('Hola Mundo')
 console.log('Jesus Daniel Vega Marvez')
-//Esto es una práctica de Git/GitHub.
+console.log('Jesus Daniel Vega Marvez #2') //Ya lo había puesto en el primer commit.
+//Esto es una práctica de Git/GitHub
